f7601988a682554cbb63600f7535c524b1df9050
apprentice1989 <apprentice1989@gmail.com>
2014-03-02 10:40:07 +0800

    Add Q1.2's answer

---
+++ chap1/ctci_1_2.cc
@@ -0,0 +1,13 @@
+void reverse(const char* src, char des[], int des_length)
+{
+	assert(des != NULL);
+	assert(src != des);
+	assert(strlen(src) + 1 <= des_length);
+
+	int length = strlen(src);
+	int index = length;
+	while(index) {
+		des[length - index] = src[index--]
+	}
+	des[length] = '\0';
+}
