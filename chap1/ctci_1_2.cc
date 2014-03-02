3da9e98045397371eb034f60513e3e9e7381263e
apprentice1989 <apprentice1989@gmail.com>
2014-03-02 10:54:40 +0800

    Fix a bug.
    des[length - index] = src[index--] ;
    这句代码除本身逻辑有问题外，整句代码的执行效果可能与数组下标的求值顺序有关，及先计算index--呢，还是先计算length -
    index，有待确认。

--- chap1/ctci_1_2.cc
+++ chap1/ctci_1_2.cc
@@ -7,7 +7,9 @@ void reverse(const char* src, char des[], int des_length)
 	int length = strlen(src);
 	int index = length;
 	while(index) {
-		des[length - index] = src[index--]
+		//des[length - index] = src[index--]
+		des[length - index] = src[index - 1];
+		--index;
 	}
 	des[length] = '\0';
 }
